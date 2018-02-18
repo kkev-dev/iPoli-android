eaca33a82baf4b244820ad44ff7107bac20eed50
Venelin Valkov <venelin@curiousily.com>
2018-02-19 00:26:48 +0200

    Fix tests

--- app/src/test/java/mypoli/android/common/redux/MiddlewareSpek.kt
+++ app/src/test/java/mypoli/android/common/redux/MiddlewareSpek.kt
@@ -1,6 +1,5 @@
 package mypoli.android.common.redux
 
-import kotlinx.coroutines.experimental.runBlocking
 import mypoli.android.common.redux.MiddleWare.Result.Continue
 import mypoli.android.common.redux.MiddleWare.Result.Stop
 import org.amshove.kluent.`should be equal to`
@@ -93,59 +92,5 @@ object MiddlewareSpek : Spek({
             result.`should be`(Stop)
         }
     }
-
-    describe("SideEffectMiddleware") {
-
-        var asyncExecutes = 0
-
-        class TestSideEffect : SideEffect<TestState> {
-            override fun canHandle(action: Action) = action is TestAction
-
-            override suspend fun execute(action: Action, state: TestState, dispatcher: Dispatcher) {
-                asyncExecutes++
-            }
-        }
-
-        beforeEachTest {
-            asyncExecutes = 0
-        }
-
-        it("should execute saga") {
-
-            runBlocking {
-                executeMiddleware(
-                    SideEffectMiddleware<TestState>(
-                        sideEffects = listOf(TestSideEffect()),
-                        coroutineContext = coroutineContext
-                    ),
-                    TestAction()
-                )
-            }
-            asyncExecutes.`should be equal to`(1)
-        }
-
-        it("should not execute saga") {
-
-            class NoExecuteSideEffect : SideEffect<TestState> {
-                override fun canHandle(action: Action) = false
-
-                override suspend fun execute(action: Action, state: TestState, dispatcher: Dispatcher) {
-                    asyncExecutes++
-                }
-            }
-
-            runBlocking {
-                executeMiddleware(
-                    SideEffectMiddleware<TestState>(
-                        sideEffects = listOf(NoExecuteSideEffect()),
-                        coroutineContext = coroutineContext
-                    ),
-                    TestAction()
-                )
-            }
-            asyncExecutes.`should be equal to`(0)
-        }
-    }
-
 })
 
